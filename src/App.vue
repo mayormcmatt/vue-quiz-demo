cc0b46710bc2cb6d2533f1e7f52426abcfcc947b
Matthew Lopez <matthew.lopez@gmail.com>
2018-05-27 12:57:29 -0700

    Simple conditional rendering of questions

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
 	<div id="app">
 		<!-- Iterate over question objects in JSON, render that many quizComponent elements, bind the individual questions and options so as to pass them as props into the component -->
-		<quizComponent v-for="question in questions" :question="question.question" :id="question.id" :options="question.options"></quizComponent>
-	</div>
+		<quizComponent v-for="question in questions" :question="question.question" :id="question.id" :options="question.options" :qNum="qNum"></quizComponent>
 
-	<!-- <button @click=""></button> -->
+		<button @click="increment">Click</button>
+	</div>
 </template>
 
 <script>
@@ -17,14 +17,20 @@
 			return {
 				jsonURL: 'https://raw.githubusercontent.com/mayormcmatt/vue-quiz-demo/master/src/assets/quizQuestions.json',
 				questions: [],
-				userResponses: []
+				userResponses: [],
+				qNum: 1
 			}
 		},
 		components: {
 			quizComponent: Quiz
 		},
+		methods: {
+			increment: function () {
+				this.qNum++
+			}
+		},
 		// Lifecycle hook: on Vue app creation run a fetch of the JSON and then set response as data property
-		created() {
+		created: function () {
 			fetch(this.jsonURL)
 				.then(res => res.json())
 				.then(res => {
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,21 +1,19 @@
 <template>
-	<div>
+	<!-- Conditionally render the question based on ID -->
+	<div v-if="id <= qNum">
+		<p>{{id}}.</p>
 		<p>{{question}}</p>
 		<quizOptions v-for="option in options" :optionText="option.option"></quizOptions>
-		<p>{{id}}</p>
 	</div>
 </template>
 
 <script>
+	// Import discreet QuizOptions component, register below in export object
 	import QuizOptions from './QuizOptions.vue'
 
 	export default {
-		props: ['question', 'id', 'options'],
-		data: function () {
-			return {
-
-			}
-		},
+		// Need the following props from the parent component: question, id, options and question number
+		props: ['question', 'id', 'options', 'qNum'],
 		components: {
 			quizOptions: QuizOptions
 		}
